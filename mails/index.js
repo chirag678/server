2128fd6121a774ec4716a97be3f35650f219cdb1
manan-m-shah <shahmananvyara@gmail.com>
2022-08-24 01:29:13 +0530

    email text

--- mails/index.js
+++ mails/index.js
@@ -15,7 +15,7 @@ export const sendWelcomeMail = async (email, link) => {
   });
 
 
-  //test
+  //* test
   // transporter.use(
   //   "compile",
   //   hbs({
@@ -32,7 +32,7 @@ export const sendWelcomeMail = async (email, link) => {
 
   const textMail = `Thank you for joining our community. We're excited to embark on this journey with you. Our talented design and engineering team is going the extra mile to make your reward even more special. We'll share the claiming page once it's live. Stay tuned!`
 
-  const htmlMail = `<h1>Thank you for joining our community.</h1><p>Thank you for joining our community. We're excited to embark on this journey with you. Our talented design and engineering team is going the extra mile to make your reward even more special. We'll share the claiming page once it's live. Stay tuned!</p>`
+  const htmlMail = `<h1>Thank you for joining our community.</h1><p>We're excited to embark on this journey with you. Our talented design and engineering team is going the extra mile to make your reward even more special. We'll share the claiming page once it's live. Stay tuned!</p>`
 
   // send mail with defined transport object
   const info = transporter.sendMail({
